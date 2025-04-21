20d0214e50d33bc5647437018d7c050e76eeea9b
danielbogdan <godanbo@googlemail.com>
2025-04-21 12:33:35 +0200

    initial version of first dataset for evaluation

--- agent.py
+++ agent.py
@@ -1,25 +1,22 @@
 import pathlib
-from abc import ABCMeta
+from abc import ABC, abstractmethod
 from langchain_openai import OpenAI
 
 
-
-class Agent(ABCMeta):
+class Agent(ABC):
 
     def __init__(self, llm_model: str, temperature: float, max_retries: int, max_tokens: int, base_url: str,
                  api_key: str):
-        super().__init__(self)
         self.llm_model = OpenAI(
-                model=llm_model,
-                temperature=temperature,
-                max_retries=max_retries,
-                max_tokens=max_tokens,
-                base_url=base_url,
-                api_key=api_key,
+            model=llm_model,
+            temperature=temperature,
+            max_retries=max_retries,
+            max_tokens=max_tokens,
+            base_url=base_url,
+            api_key=api_key,
         )
 
-
-
+    @abstractmethod
     def eval(self, question: str, dataset: pathlib.Path, additional_info: list[dict]) -> str:
         """
         Evaluate the given data using the LLM.
@@ -30,5 +27,3 @@ class Agent(ABCMeta):
             :return: The response from the LLM
         """
         pass
-
-
