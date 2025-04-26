a32cdd055c1456426246a587d1aac7a248b1d8ad
F4zination <minecraft.neot@gmail.com>
2025-04-26 17:51:22 +0200

    SCRUM-33 updated serialization_agent.py to use updated Agent

--- serialization/serialization_agent.py
+++ serialization/serialization_agent.py
@@ -10,9 +10,10 @@ class SerializationAgent(Agent):
     """
 
     def __init__(self, llm_model: str, temperature: float, max_retries: int, max_tokens: int, base_url: str,
-                 api_key: str):
-        super().__init__(llm_model, temperature, max_retries, max_tokens, base_url, api_key)
+                 api_key: str, system_prompt: str = None, format_to_convert_to: TableFormat = TableFormat.NATURAL):
+        super().__init__(llm_model, temperature, max_retries, max_tokens, base_url, api_key, system_prompt)
         self.converter = Converter()
+        self.format_to_convert_to = format_to_convert_to
 
 
     def eval(self, question: str, dataset: pathlib.Path, additional_info: list[dict]) -> str:
@@ -24,10 +25,10 @@ class SerializationAgent(Agent):
         :return:
         """
         # Perform the serialization task using the Converter class
-        converted_content = self.converter.convert(dataset, TableFormat.HTML, False)
+        converted_content = self.converter.convert(dataset, self.format_to_convert_to, False)
 
         # Use the LLM to generate a response based on the question and converted content
-        response = self.llm_model.invoke(question + converted_content)
+        response = self.llm_model.invoke(self.system_prompt + question + converted_content)
 
         return response
 
