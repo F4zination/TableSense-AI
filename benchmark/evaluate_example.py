f6506c9e581d47213c2b43607fae53eadde369b8
danielbogdan <godanbo@googlemail.com>
2025-05-16 14:06:58 +0200

    fix connection error and finish metric stuff

--- benchmark/evaluate_example.py
+++ benchmark/evaluate_example.py
@@ -1,8 +1,9 @@
 from evaluate import load
 
-from benchmark.evaluator.dataset_definition import SimpleTest
+from benchmark.evaluator.dataset_definition import SimpleTest, WikiTableQuestions
 from benchmark.evaluator.evaluator import Evaluator
 from benchmark.evaluator.evaluator import EvalConfig
+from benchmark.evaluator.metrics.exact_match_metric import ExactMatchMetric
 from serialization.serialization_agent import SerializationAgent
 
 # Create your agent instance to test
@@ -12,13 +13,9 @@ agent = SerializationAgent(llm_model="/models/mistral-nemo-12b",
                            max_tokens=2048,
                            base_url="http://80.151.131.52:9180/v1",
                            api_key="THU-I17468S973-Student-24-25-94682Y1315")
-# Load Exact Match Metrik
-exact_match = load("exact_match")
-
 
 # Configure your evaluation instance
-config = EvalConfig([SimpleTest()], False, [exact_match]
-)
+config = EvalConfig([SimpleTest()], [ExactMatchMetric()], False)
 evaluator = Evaluator(config, agent)
 
 # Start the evaluation process
--- benchmark/evaluator/metrics/metric.py
+++ benchmark/evaluator/metrics/metric.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Metric(ABC):
+
+    def __init__(self, metric_name):
+        self.metric_name = metric_name
+
     @abstractmethod
-    def compute(self, prediction: list, ground_truth: list) -> float:
+    def compute(self, predictions: list, references: list) -> float:
         ...
