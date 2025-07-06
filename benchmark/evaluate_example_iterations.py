9e5e620810d9629defccf5eb7f98869f65d716ef
danielbogdan <godanbo@googlemail.com>
2025-07-06 11:36:32 +0200

    make multiple iterations optional

--- benchmark/evaluate_example_iterations.py
+++ benchmark/evaluate_example_iterations.py
@@ -6,6 +6,8 @@ from benchmark.evaluator.evaluator import Evaluator
 from benchmark.evaluator.evaluator import EvalConfig
 from tablesense_ai.agent.serialization.serialization_agent import SerializationAgent
 
+iterations = 2
+
 # Create your agent instance to test
 agent = SerializationAgent(llm_model="/models/mistral-nemo-12b",
                            temperature=0,
@@ -14,14 +16,21 @@ agent = SerializationAgent(llm_model="/models/mistral-nemo-12b",
                            base_url="http://80.151.131.52:9180/v1",
                            api_key="THU-I17468S973-Student-24-25-94682Y1315", verbose=True)
 
-# Configure your evaluation instance
-config = EvalConfig([SimpleTest(), WikiTableQuestions()], True, True)
-evaluator = Evaluator(config, agent)
+complete_results = []
 
-# Start the evaluation process
-results = evaluator.evaluate()
+for i in range(iterations):
+    # Configure your evaluation instance
+    config = EvalConfig([SimpleTest(), WikiTableQuestions()], True, True)
+    evaluator = Evaluator(config, agent)
 
-print(results)
+    # Start the evaluation process
+    results = evaluator.evaluate()
+
+    print(results)
+    complete_results.append(results)
+
+averaged = helper.average_results(complete_results)
+print(averaged)
 
 with open("evaluation_results.pkl", "wb") as f:
-    pickle.dump(results, f)
+    pickle.dump(complete_results, f)
